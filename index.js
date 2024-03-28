ab03157e5895d5aa7658031bc03a63d1c10343cd
Abhishek Raj <118152707+Abhishekbotx@users.noreply.github.com>
2024-03-29 00:56:08 +0530

    integrated database and integrated normal css

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express=require('express')
 const http = require('http');
 const socketio = require('socket.io');
-
+const Chat=require('./model/Chat')
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const connect=require('./config/database')
 
 
 
@@ -16,27 +17,44 @@ io.on('connection', (socket) => {
         socket.join(data.roomid);
     });
 
-    socket.on('msg_send', (data) => {
+    socket.on('msg_send', async(data) => {
+        const chat=await Chat.create({
+            roomId: data.roomid,
+            user: data.username,
+            content: data.msg
+        })
         console.log(data);
         io.to(data.roomid).emit('msg_rcvd', data);
     });
+
+    socket.on('typing',(data)=>{
+        socket.broadcast.to(data.roomid).emit('someonetyping')
+    })
 });
 
 app.set('view engine', 'ejs');
 app.use('/', express.static(__dirname + '/public'));
 
-app.get('/chat/:roomid', (req, res) => {
+
+
+app.get('/chat/:roomid', async (req, res) => {
+    const chats = await Chat.find({
+        roomId: req.params.roomid
+    }).select('content user');//selecting the content and the user
+    console.log(chats);
     res.render('index', {
-        name: 'Sanket',
-        id: req.params.roomid
+        name: 'Abhishek',
+        id: req.params.roomid,
+        chats: chats
     });
 });
 
 
 app.use('/', express.static(__dirname + '/public'));
 
-server.listen(3001, () => {
+server.listen(3001, async() => {
     console.log('Server started');
+    await connect()
 });
 
 
